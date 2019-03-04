0ab27ed32999c3568178b7b39762bc8ac3eef806
Marcus Fedarko <mfedarko@ucsd.edu>
2019-03-04 11:45:42 -0800

    FIX: Use header=0 in pd.read_csv()
    header=True isn't a valid argument to pd.read_csv(), and it's making
    pandas angry when rankratioviz/Q2 try to process the differentials
    input.
    
    (header=True is a valid argument to pd.to_csv(), though -- this mixup
    has apparently occurred before, per google)

--- songbird/q2/_transformer.py
+++ songbird/q2/_transformer.py
@@ -20,7 +20,7 @@ def _2(obj: qiime2.Metadata) -> SongbirdStatsFormat:
 @plugin.register_transformer
 def _3(ff: DifferentialFormat) -> pd.DataFrame:
     df = pd.read_csv(str(ff), sep='\t', comment='#', skip_blank_lines=True,
-                     header=True, dtype=object)
+                     header=0, dtype=object)
     return df
 
 
